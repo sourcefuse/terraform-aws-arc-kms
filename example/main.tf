4c08f83bd29970658019152a064b06d36e232a2a
mayank0202 <mayank.sharma@sourcefuse.com>
2024-04-20 08:36:36 +0530

    fixed workflow v5

--- example/main.tf
+++ example/main.tf
@@ -1,17 +1,3 @@
-terraform {
-  required_version = "~> 1.4"
-
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.0"
-    }
-  }
-}
-
-provider "aws" {
-  region = var.region
-}
 # data "aws_caller_identity" "current" {}
 module "tags" {
   source  = "sourcefuse/arc-tags/aws"
